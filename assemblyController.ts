b62aa57ec64ab0c8d2e8aa6e7619810c2cbde089
KonH <konh@yandex.ru>
2017-08-26 14:40:58 +0700

    Register abbrs usage;

--- assemblyController.ts
+++ assemblyController.ts
@@ -1,4 +1,39 @@
+class ArgHolder {
+	rawValue : string;
+	readyValue : number;
+	size : number;
+
+	constructor(rawValue : string, size : number) {
+		this.rawValue = rawValue;
+		this.size = size;
+	}
+}
+
+class AssemblyHolder {
+	handler : HandlerBase;
+	argHolders : ArgHolder[] = [];
+
+	constructor(handler : HandlerBase, args : string[]) {
+		this.handler = handler;
+		let argIndex = 0;
+		for (var i = 0; i < this.handler.shortArgs; i++) {
+			this.argHolders.push(new ArgHolder(args[argIndex], Command.shortArgSize));
+			argIndex++;
+		}
+		for (var i = 0; i < this.handler.wideArgs; i++) {
+			this.argHolders.push(new ArgHolder(args[argIndex], Command.wideArgSize));
+			argIndex++;
+		}
+	}
+
+	getHeader() {
+		return this.handler.header;
+	}
+}
+
 class AssemblyController {
+	static InputRadix : number = 10;
+
 	private assemblyInput : HTMLTextAreaElement;
 	private machineCodeInput : HTMLTextAreaElement;
 	private generateButton : HTMLButtonElement;
@@ -15,42 +50,110 @@ class AssemblyController {
 	}
 
 	private generateMachineCode(assemblyCode : string) {
-		let machineCode = "";
 		let lines = assemblyCode.split('\n');
-		lines.forEach(line => machineCode += this.convertLine(line));
+		let holders = this.createHolders(lines);
+		this.setupAllArgValues(holders);
+		let machineCode = this.getMachineCode(holders);
 		this.machineCodeInput.value = machineCode;
 	}
 
-	convertLine(line : string) : string {
+	private createHolders(lines : string[]) : AssemblyHolder[] {
+		let holders : AssemblyHolder[] = [];
+		lines.forEach(line => {
+			let holder = this.convertLine(line);
+			if	(holder != null) {
+				holders.push(holder);
+			}
+		});
+		return holders;
+	}
+
+	convertLine(line : string) : AssemblyHolder {
 		let parts = line.split(' ');
 		Logger.write("assemblyController", "line parts: " + parts);
 		if (parts.length > 0) {
 			var name = parts[0];
 			var args = parts.splice(1);
-			return this.createCommand(name, args);
+			return this.createHolder(name, args);
 		}
-		return "";
+		return null;
 	}
 
-	createCommand(name : string, args : string[]) : string {
+	private createHolder(name : string, args : string[]) : AssemblyHolder {
 		Logger.write("assemblyController", "createCommand from '" + name + "', " + args);
 		let handler = this.helper.findHandlerByName(name);
 		if (handler != null) {
-			let text = "";
-			text += handler.header.toString(2) + " ";
 			let argCount = handler.shortArgs + handler.wideArgs;
 			if (args.length == argCount ) {
-				args.forEach(arg => 
-					text += arg + " "
-				);
-				text += "	";
-				return text;	
+				return new AssemblyHolder(handler, args);
 			} else {
 				Logger.write("assemblyController", "wrong arg count: " + args.length + ", but " + argCount + " expected");
 			}
 		} else {
 			Logger.write("assemblyController", "unknown command: '" + name + "'");
 		}
-		return "";
+		return null;
+	}
+
+	private setupAllArgValues(holders : AssemblyHolder[]) {
+		holders.forEach(holder => {
+			this.setupArgValues(holder);
+		});
+	}
+
+	private setupArgValues(holder : AssemblyHolder) {
+		holder.argHolders.forEach(item => this.setupArgValue(item));
+	}
+
+	private parseInt(value : string) : number {
+		return parseInt(value, AssemblyController.InputRadix);
+	}
+
+	private getRegisterAddrByName(name : string) : number {
+		return this.parseInt(name.slice(1)) - 1;
+	}
+
+	private tryConvertRegisterAbbr(arg : ArgHolder) : boolean {
+		let value = arg.rawValue;
+		if (value.startsWith("%")) {
+			arg.readyValue = this.getRegisterAddrByName(value);
+			return true;
+		}
+		return false;
+	}
+
+	private setupArgValue(arg : ArgHolder) {
+		if(!this.tryConvertRegisterAbbr(arg)) {
+			arg.readyValue = this.parseInt(arg.rawValue);
+		}
+	}
+
+	private toProperCodeString(value : number, len : number) : string {
+		let str = value.toString(2);
+		while (str.length < len) {
+			str = "0" + str;
+		}
+		return str;
+	}
+
+	private convertArgs(args : ArgHolder[]) : string {
+		let str = "";
+		args.forEach(arg => {
+			let argValue = this.toProperCodeString(arg.readyValue, arg.size);
+			argValue += " ";
+			str += argValue;
+		});
+		return str;
+	}
+
+	private getMachineCode(holders : AssemblyHolder[]) : string {
+		let machineCode = "";
+		holders.forEach(holder => {
+			machineCode += this.toProperCodeString(holder.getHeader(), Command.headerSize);
+			machineCode += " ";
+			machineCode += this.convertArgs(holder.argHolders);
+			machineCode += "  ";
+		});
+		return machineCode;
 	}
 }
--- js/assemblyController.js
+++ js/assemblyController.js
@@ -1,3 +1,29 @@
+var ArgHolder = (function () {
+    function ArgHolder(rawValue, size) {
+        this.rawValue = rawValue;
+        this.size = size;
+    }
+    return ArgHolder;
+}());
+var AssemblyHolder = (function () {
+    function AssemblyHolder(handler, args) {
+        this.argHolders = [];
+        this.handler = handler;
+        var argIndex = 0;
+        for (var i = 0; i < this.handler.shortArgs; i++) {
+            this.argHolders.push(new ArgHolder(args[argIndex], Command.shortArgSize));
+            argIndex++;
+        }
+        for (var i = 0; i < this.handler.wideArgs; i++) {
+            this.argHolders.push(new ArgHolder(args[argIndex], Command.wideArgSize));
+            argIndex++;
+        }
+    }
+    AssemblyHolder.prototype.getHeader = function () {
+        return this.handler.header;
+    };
+    return AssemblyHolder;
+}());
 var AssemblyController = (function () {
     function AssemblyController(assembly, machine, generate, helper) {
         var _this = this;
@@ -8,11 +34,22 @@ var AssemblyController = (function () {
         this.helper = helper;
     }
     AssemblyController.prototype.generateMachineCode = function (assemblyCode) {
-        var _this = this;
-        var machineCode = "";
         var lines = assemblyCode.split('\n');
-        lines.forEach(function (line) { return machineCode += _this.convertLine(line); });
+        var holders = this.createHolders(lines);
+        this.setupAllArgValues(holders);
+        var machineCode = this.getMachineCode(holders);
         this.machineCodeInput.value = machineCode;
+    };
+    AssemblyController.prototype.createHolders = function (lines) {
+        var _this = this;
+        var holders = [];
+        lines.forEach(function (line) {
+            var holder = _this.convertLine(line);
+            if (holder != null) {
+                holders.push(holder);
+            }
+        });
+        return holders;
     };
     AssemblyController.prototype.convertLine = function (line) {
         var parts = line.split(' ');
@@ -20,23 +57,17 @@ var AssemblyController = (function () {
         if (parts.length > 0) {
             var name = parts[0];
             var args = parts.splice(1);
-            return this.createCommand(name, args);
+            return this.createHolder(name, args);
         }
-        return "";
+        return null;
     };
-    AssemblyController.prototype.createCommand = function (name, args) {
+    AssemblyController.prototype.createHolder = function (name, args) {
         Logger.write("assemblyController", "createCommand from '" + name + "', " + args);
         var handler = this.helper.findHandlerByName(name);
         if (handler != null) {
-            var text_1 = "";
-            text_1 += handler.header.toString(2) + " ";
             var argCount = handler.shortArgs + handler.wideArgs;
             if (args.length == argCount) {
-                args.forEach(function (arg) {
-                    return text_1 += arg + " ";
-                });
-                text_1 += "	";
-                return text_1;
+                return new AssemblyHolder(handler, args);
             }
             else {
                 Logger.write("assemblyController", "wrong arg count: " + args.length + ", but " + argCount + " expected");
@@ -45,7 +76,65 @@ var AssemblyController = (function () {
         else {
             Logger.write("assemblyController", "unknown command: '" + name + "'");
         }
-        return "";
+        return null;
     };
+    AssemblyController.prototype.setupAllArgValues = function (holders) {
+        var _this = this;
+        holders.forEach(function (holder) {
+            _this.setupArgValues(holder);
+        });
+    };
+    AssemblyController.prototype.setupArgValues = function (holder) {
+        var _this = this;
+        holder.argHolders.forEach(function (item) { return _this.setupArgValue(item); });
+    };
+    AssemblyController.prototype.parseInt = function (value) {
+        return parseInt(value, AssemblyController.InputRadix);
+    };
+    AssemblyController.prototype.getRegisterAddrByName = function (name) {
+        return this.parseInt(name.slice(1)) - 1;
+    };
+    AssemblyController.prototype.tryConvertRegisterAbbr = function (arg) {
+        var value = arg.rawValue;
+        if (value.startsWith("%")) {
+            arg.readyValue = this.getRegisterAddrByName(value);
+            return true;
+        }
+        return false;
+    };
+    AssemblyController.prototype.setupArgValue = function (arg) {
+        if (!this.tryConvertRegisterAbbr(arg)) {
+            arg.readyValue = this.parseInt(arg.rawValue);
+        }
+    };
+    AssemblyController.prototype.toProperCodeString = function (value, len) {
+        var str = value.toString(2);
+        while (str.length < len) {
+            str = "0" + str;
+        }
+        return str;
+    };
+    AssemblyController.prototype.convertArgs = function (args) {
+        var _this = this;
+        var str = "";
+        args.forEach(function (arg) {
+            var argValue = _this.toProperCodeString(arg.readyValue, arg.size);
+            argValue += " ";
+            str += argValue;
+        });
+        return str;
+    };
+    AssemblyController.prototype.getMachineCode = function (holders) {
+        var _this = this;
+        var machineCode = "";
+        holders.forEach(function (holder) {
+            machineCode += _this.toProperCodeString(holder.getHeader(), Command.headerSize);
+            machineCode += " ";
+            machineCode += _this.convertArgs(holder.argHolders);
+            machineCode += "  ";
+        });
+        return machineCode;
+    };
+    AssemblyController.InputRadix = 10;
     return AssemblyController;
 }());
